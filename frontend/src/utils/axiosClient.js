d75d8cbb7fe64ae3c5931060b1c960146c6b135d
Avinash kumar <jhaclsxn8126@gmail.com>
2026-01-12 11:01:16 +0530

    connection issue

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -2,17 +2,19 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
-  withCredentials: true,
+  withCredentials: true, // allow cookies if needed in future
 });
 
 axiosClient.interceptors.request.use((config) => {
   const auth = localStorage.getItem("auth");
+
   if (auth) {
     const { token } = JSON.parse(auth);
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
   }
+
   return config;
 });
 
